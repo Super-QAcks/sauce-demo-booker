5a25109d63b8ee8c8bafef7636153a29beabee13
Francisco Mondragón <fjgm11@hotmail.com>
2026-03-20 19:07:40 -0600

    Invalid login with invalid email and password

--- ui/pom/data/credentials.ts
+++ ui/pom/data/credentials.ts
@@ -1,4 +1,6 @@
 export const LOGIN_CREDENTIALS = {
 	STANDARD_USER: process.env.STANDARD_USER || "default user",
 	PASSWORD: process.env.PASSWORD || "default password",
+	INVALID_USER: process.env.INVALID_USER || "invalid user",
+	INVALID_PASSWORD: process.env.INVALID_PASSWORD || "invalid password",
 };
--- ui/tests/helpers/loginFixture.ts
+++ ui/tests/helpers/loginFixture.ts
@@ -12,3 +12,14 @@ export const test = base.extend<{ loginPage: LoginPage }>({
 		await use(loginPage);
 	},
 });
+
+export const invalidLoginTest = base.extend<{ loginPage: LoginPage }>({
+	loginPage: async ({ page }, use) => {
+		const loginPage = new LoginPage(page);
+		await loginPage.goto();
+		await loginPage.waitForRoot();
+		await loginPage.invalidLogin();
+		await loginPage.waitForErrorMessage();
+		await use(loginPage);
+	},
+});
