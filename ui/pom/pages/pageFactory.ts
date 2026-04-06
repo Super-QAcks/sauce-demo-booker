a511388ede8f110c8a7fbc01e218dd52f7b62546
Francisco Mondragón <fjgm11@hotmail.com>
2026-04-06 12:44:13 -0600

    scroll down test

--- ui/pom/pages/pageFactory.ts
+++ ui/pom/pages/pageFactory.ts
@@ -1,13 +1,24 @@
-import { Page } from "@playwright/test";
+import { Page, Locator } from "@playwright/test";
 
 export class PageFactory {
 	protected page: Page;
+	readonly scrollUpButton: Locator;
 
 	constructor(page: Page) {
 		this.page = page;
+		this.scrollUpButton = page.locator("#scrollUp");
 	}
 
 	async goto(url: string) {
 		await this.page.goto(url, { waitUntil: "commit", timeout: 60000 });
+	}
+
+	async clickScrollUp() {
+		await this.scrollUpButton.click();
+		await this.scrollUpButton.waitFor({ state: "hidden" });
+	}
+
+	async waitForRoot() {
+		await this.scrollUpButton.waitFor({ state: "visible" });
 	}
 }
---
+++ ui/tests/home.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from "@playwright/test";
+import { HomePage } from "../pom/pages/homePage";
+import { FooterComponent } from "../pom/component/footeer.component";
+import { URL_BASE } from "../pom/data/urls";
+
+test.describe("Home Page Tests.", async () => {
+	test("Test Case 25: Verify Scroll Up using 'Arrow' button and Scroll Down functionality: ", async ({
+		page,
+	}) => {
+		const homePage = new HomePage(page);
+		const footerComponent = new FooterComponent(page);
+
+		await test.step("Launch browser and Navigate to url 'http://automationexercise.com'", async () => {
+			await homePage.goto(URL_BASE);
+		});
+
+		await test.step("Verify that home page is visible successfully", async () => {
+			await homePage.waitForRoot();
+		});
+
+		await test.step("Scroll down page to bottom", async () => {
+			await page.evaluate(() => {
+				window.scrollTo(0, document.body.scrollHeight);
+			});
+		});
+
+		await test.step("Verify 'SUBSCRIPTION' is visible", async () => {
+			await expect(footerComponent.titleWidget).toBeVisible();
+		});
+
+		await test.step("Click on arrow at bottom right side to move upward", async () => {
+			await homePage.clickScrollUp();
+		});
+
+		await test.step("Verify that page is scrolled up and 'Full-Fledged practice website for Automation Engineers' text is visible on screen", async () => {
+			await expect(homePage.activeSliderH2).toBeVisible();
+		});
+	});
+});
